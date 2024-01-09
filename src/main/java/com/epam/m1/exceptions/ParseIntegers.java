444b432193a2020dd36c2457a4abb8d8f8c64988
Albert <assasinsalbert@gmail.com>
2024-01-09 23:58:03 +0500

    code completed

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,11 +21,16 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum=sum+number;
+            }
+            catch (NumberFormatException ignored){
+                justWords=justWords+next+" ";
+            }
         }
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words: " + justWords);
     }
 }
 
